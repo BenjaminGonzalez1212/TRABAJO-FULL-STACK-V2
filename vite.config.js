c3544d5b2613bf0b6a94afb7ca8c0b6595592364
BenjaminGonzalez1212 <benjam.gonzalezl@duocuc.cl>
2025-10-20 00:54:01 -0300

    first commit

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/TRABAJO-FULL-STACK-V2/'
+  base: "/Trabajo-Full-Stack-V2"
 })
