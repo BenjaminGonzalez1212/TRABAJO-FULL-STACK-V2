9c02945cd164b6f775260be650a38a8f3f27ad96
BenjaminGonzalez1212 <benjam.gonzalezl@duocuc.cl>
2025-10-20 01:20:28 -0300

    cosas
    github pages no funcionales, se deja en version estable

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/Trabajo-Full-Stack-V2"
+  base: "/TRABAJO-FULL-STACK-V2"
 })
